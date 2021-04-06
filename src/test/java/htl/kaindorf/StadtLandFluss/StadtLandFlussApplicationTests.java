4b6d5eb61c411d3a0897578d789fcd10bd6d11f9
paulweiss18 <paul.weisz181@gmail.com>
2021-04-06 11:34:49 +0200

    initial commit

---
+++ src/test/java/htl/kaindorf/StadtLandFluss/StadtLandFlussApplicationTests.java
@@ -0,0 +1,13 @@
+package htl.kaindorf.StadtLandFluss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadtLandFlussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
